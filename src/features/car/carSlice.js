e3b429bfa3ee1746326b15151df9b734de92ba99
malikhakeem <109516112+malikhakeem@users.noreply.github.com>
2022-09-19 19:40:55 +0100

    Update carSlice.js

--- src/features/car/carSlice.js
+++ src/features/car/carSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  car: ["Model S", "Model 3", "Model X", "Model Y"],
+  cars: ["Model S", "Model 3", "Model X", "Model Y"],
 };
 
 const carSlice = createSlice({
